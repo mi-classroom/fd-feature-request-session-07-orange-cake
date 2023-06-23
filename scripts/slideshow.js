0bb6d4114521ca1b5eb20080d3716e46da72332e
Robin Kraft <92304667+Roba42@users.noreply.github.com>
2023-06-23 12:09:04 +0200

    test1-final

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -41,3 +41,16 @@ const slideshow = (ele) => {
 window.addEventListener("DOMContentLoaded", (event) => {
     slideshow(slideshowElement);
 });
+
+var prevScrollpos = window.pageYOffset;
+var internNav = document.querySelector('.intern-nav');
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    internNav.style.transform = 'translateY(0)';
+  } else {
+    internNav.style.transform = 'translateY(100%)';
+  }
+  prevScrollpos = currentScrollPos;
+};
